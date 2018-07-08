3adb75780d04a8a81a42093a63bae9c94ea96cba
Expert <expert2006@ukr.net>
2018-07-08 09:41:37 +0300

    App React day2

--- src/App.js
+++ src/App.js
@@ -3,37 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+
+`
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+
+`
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+
+`
 
 class App extends Component {
   render() {
     return (
-               <div className="wrapper">
-                  <SayFullName name="Vasily" surname="Gukovskiy" link="vk.com" />
-                  <SayFullName name="Anna" surname="Antonova" link="facebook.com" />
-                  <SayFullName name="Sergey" surname="Petrovich" link="#" />
-                  <Hello name="World!" />
-              </div>
+        <div className="App">
+            <HeaderWrapper>
+                <Grid>
+                    <Header />
+                </Grid>
+            </HeaderWrapper>
+            <MenuWrapper>
+                <Grid>
+                    <Menu />
+                </Grid>
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main/>     
+                </Grid>
+            </MainWrapper>       
+        </div>      
     );
   }
 }
 
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Мое имя {props.name}, Фамилия - {props.surname}</h1>
-      <a href={props.link}>Ссылка на мой профиль</a>
-    </div>
-    )
-} 
-function Hello(props) {
-  return (
-      <div>
-          <h2>Hello {props.name}</h2>
-      </div>
-  )
-}
 
 export default App;
